2cf637be8cdf7581c4daff7b7a471164c3618d42
lailadwikartikasari <akunc4086@gmail.com>
2024-10-01 13:43:33 +0700

    update2

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "obesityku"))
         self.label_3.setText(_translate("MainWindow", "OBESITYKU"))
         self.label_4.setText(_translate("MainWindow", "Apakah Anda Mengalami Obesitas? "))
         self.label_6.setText(_translate("MainWindow", "Usia"))
